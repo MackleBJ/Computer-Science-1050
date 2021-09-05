b97cd112a12845ebe0cc958a325f5351d51c9ea3
Brett <67874400+MackleBJ@users.noreply.github.com>
2021-09-05 10:48:52 -0500

    Update prelab3.c

--- Lab 3/prelab3.c
+++ Lab 3/prelab3.c
@@ -1,1 +1,25 @@
+/*********************
+* Author: Brett M.
+* Class: CS 1050
+* Semester: Fall 2021
+* *******************/
 
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 1;
+
+    while (number < 11)
+    {
+        if (number % 2 == 0)
+        {
+            printf("even - %d\n", number);
+        }
+        else
+        {
+            printf("odd - %d\n", number);
+        }
+        number ++;
+    }
+}
