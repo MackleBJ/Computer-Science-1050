04fda719f16dfa1acbeb08902d8f858556d25fa4
Brett <67874400+MackleBJ@users.noreply.github.com>
2021-09-15 20:07:57 -0500

    Update prelab4.c

--- Lab 4/prelab4.c
+++ Lab 4/prelab4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int limit, cubedNumber, counter;
+
+    printf("Please enter an integer: ");
+    scanf("%d", &limit);
+
+    for (counter = 0; (counter*counter*counter) <= limit; counter++)
+    {
+        cubedNumber = counter*counter*counter;
+        if (cubedNumber % 3 == 0)
+        {
+            printf("%d*\n", cubedNumber);
+        }
+        else
+        {
+            printf("%d\n", cubedNumber);
+        }
+    }
+}
