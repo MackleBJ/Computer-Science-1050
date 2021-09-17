33c2175121c024edd2a2e8b8085624cb7a159f12
Brett <67874400+MackleBJ@users.noreply.github.com>
2021-09-17 15:48:12 -0500

    Create lab4.c

---
+++ Lab 4/lab4.c
@@ -0,0 +1,34 @@
+/**************************************************
+ * Course:   CS 1050
+ * Semester: Fall 2021
+ * Date:     September 17, 2021
+ * Lab:      Lab 4 
+ * Author:   Brett McElvain
+ * Pawprint: bjm2cd
+ * ***********************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /* Initialize variables */
+    int counter, start = 0, limit = 0;
+
+    /* Ask the user for Start and Limit Integers */
+    for (;start < 1 || start > 150;)
+    {
+        printf("Enter Start: ");
+        scanf("%d", &start);
+    }
+    for (;limit < 1 || limit > 150;)
+    {
+        printf("Enter Limit: ");
+        scanf("%d", &limit);
+    }
+
+    /* Count from Start to Limit by increments of 3 */
+    for (counter = start; counter <= limit; counter +=3)
+    {
+        printf("%d ", counter);
+    }
+}
